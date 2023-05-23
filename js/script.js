950147ad721d8125dddd2f36523ea5c41ac2705d
AbdoNayer <abdelwahabnayer20@gmail.com>
2023-05-23 22:34:14 +0300

    add-my-services

--- js/script.js
+++ js/script.js
@@ -24,6 +24,16 @@ $(document).ready(function () {
         $(this).parents('.uploaded-block').remove();
     });
 
+    // // ADD IMAGE
+    $('.video-uploader').change(function (event){
+        $(this).parents('.images-upload-block').find('.upload-area').append('<div class="uploaded-block"><video src="'+ URL.createObjectURL(event.target.files[0]) +'" controls></video><button class="close"><i class="icon-trash text-white"></i></button></div>');
+    });
+    
+    // REMOVE IMAGE
+    $('.images-upload-block').on('click', '.close',function (){
+        $(this).parents('.uploaded-block').remove();
+    });
+
     // Click Open Nav In Mobile
     $(document).on("click", ".click-nav, .overlay, .modal-country",function() {
         $(".handle").toggleClass("closed");
@@ -35,6 +45,13 @@ $(document).ready(function () {
     // Click Remove Class For All Section
     $(document).on("click", ".overlay-absolute",function() {
         $("body").removeClass("overflow-hidden");
+    });
+
+    // Upload Files
+    $('#file-upload').change(function() {
+        var i = $(this).prev('label').clone();
+        var file = $('#file-upload')[0].files[0].name;
+        $(this).prev('label').text(file);
     });
 
     // Animation Page
